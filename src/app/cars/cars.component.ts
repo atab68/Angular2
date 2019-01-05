c4a1083c66e9938f10b71dee3b3f35181b9a5b8d
Aselt <atabaldieva86@gmail.com>
2019-01-05 11:49:02 -0600

    directive *ngFor examples

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -9,11 +9,15 @@ export class CarsComponent {
 
  carName = '';
  addCarStatus = false;
+ cars = ['Ford', 'Audi', 'BMW'];
+ items = [{id: 3, name: 'item 3'},{id: 6, name: 'item 6'},{id: 9, name: 'item 9'}];
 
   constructor() {}
 
   addCar() {
     this.addCarStatus = true;
+    this.cars.push(this.carName);
+    this.carName = '';
   }
 
  
